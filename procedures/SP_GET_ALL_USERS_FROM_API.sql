40911e9eb43920006236f9f3b61847cd8dcb2621
trankhanh040147 <trankhanh040147@gmail.com>
2023-07-31 05:59:46 +0700

    Update SP_GET_ALL_USERS_FROM_API.sql

--- procedures/SP_GET_ALL_USERS_FROM_API.sql
+++ procedures/SP_GET_ALL_USERS_FROM_API.sql
@@ -31,7 +31,7 @@ n_gender_str NVARCHAR2(100);
 n_gender number;
 n_country NVARCHAR2(100);
 n_title NVARCHAR2(100);
-
+--
 
 -- BANK ACCOUNT fields
 b_acc_num NVARCHAR2(50);
@@ -105,7 +105,7 @@ BEGIN
         n_managerPosition := apex_json.get_varchar2('[%d].ManagerPosition', i);
         --
         n_birth_date := TO_DATE(apex_json.get_varchar2('[%d].BirthDate', i), 'YYYY-MM-DD"T"HH24:MI:SS');
-        n_start_date := TO_DATE(apex_json.get_varchar2('[%d].EmployeeStartDate', i), 'YYYY-MM-DD"T"HH24:MI:SS');
+        n_start_date := TO_DATE(apex_json.get_varchar2('[%d].EmployeeStartDate', i), 'YYYY-MM-DD"T"HH24:MI:SS"Z"');
         n_gender_str := apex_json.get_varchar2('[%d].Gender', i);
         n_gender := CASE 
             WHEN n_gender_str LIKE 'Mr.%' THEN 1
@@ -124,12 +124,15 @@ BEGIN
         If l_count_idemp > 0 Then
             UPDATE EMPLOYEES SET EMPLOYEE_ID = i, FIRST_NAME = n_firstname, LAST_NAME = n_lastname,
                                 FULL_NAME = n_name, PHONE_NUMBER = n_phone,  PERSONAL_EMAIL = n_email,
-                                EMPLOYEE_CODE = n_code, USER_NAME = n_email, MANAGER_ID = n_managernumber ,POSITION_ID = n_positionId,MANAGER_POSITION_ID= n_managerPosition
+                                EMPLOYEE_CODE = n_code, USER_NAME = n_email, MANAGER_ID = n_managernumber ,POSITION_ID = n_positionId,MANAGER_POSITION_ID= n_managerPosition,
+                                DOB = n_birth_date, START_DATE = n_start_date, GENDER = n_gender, COUNTRY = n_country, TITLE = n_title    
             WHERE ID = i ;
 
         Else
-            INSERT INTO EMPLOYEES(ID, EMPLOYEE_ID, FIRST_NAME, LAST_NAME, FULL_NAME, PHONE_NUMBER, PERSONAL_EMAIL, EMPLOYEE_CODE, USER_NAME, MANAGER_ID,POSITION_ID,MANAGER_POSITION_ID)
-            VALUES(i, i, n_firstname, n_lastname, n_name, n_phone, n_email, n_code, n_email, n_managernumber,n_positionId,n_managerPosition);
+            INSERT INTO EMPLOYEES(ID, EMPLOYEE_ID, FIRST_NAME, LAST_NAME, FULL_NAME, PHONE_NUMBER, PERSONAL_EMAIL, EMPLOYEE_CODE, USER_NAME, 
+                MANAGER_ID,POSITION_ID,MANAGER_POSITION_ID, DOB, START_DATE, GENDER, COUNTRY, TITLE)
+            VALUES(i, i, n_firstname, n_lastname, n_name, n_phone, n_email, n_code, n_email, n_managernumber,n_positionId,n_managerPosition,
+                n_birth_date, n_start_date, n_gender, n_country, n_title);
 
         End If;
 
