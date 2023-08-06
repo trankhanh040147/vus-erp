ad6d3e547ca4f615c50e94c91f818ddbbecd3a2e
trankhanh040147 <trankhanh040147@gmail.com>
2023-08-07 00:11:40 +0700

    Update Update_request.sql

--- SQL QUERY/leave_request/Update_request.sql
+++ SQL QUERY/leave_request/Update_request.sql
@@ -3,17 +3,20 @@ v_request_id number;
 v_action nvarchar2(100);
 v_day_temp number:=0;
 
-r_name_type nvarchar(200);
-r_AdjustedHours float(10);
-r_AdjustmentType nvarchar(100);
+r_name_type nvarchar2(100);
+r_AdjustedHours FLOAT(10);
+r_AdjustmentType number;
 r_TransactionDate date;
-r_Description nvarchar(200);
-r_EmployeeCode nvarchar(100);
-r_HRMAbsenceCodeId nvarchar(100);
-r_IDPortal number;
+r_Description nvarchar2(100);
+r_EmployeeCode nvarchar2(100);
+r_HRMAbsenceCodeId nvarchar2(100);
+r_IDPortal NUMBER;
+
+
 
 begin
 v_request_id := apex_application.g_x01;
+
 --v_action := apex_application.g_x02;
 --update EMPLOYEE_REQUESTS set IS_DEL = 1 where request_id = v_request_id;
 /*
@@ -39,32 +42,35 @@ for rec in (select * from EMP_ANNUAL_LEAVE eal join EMPLOYEE_REQUESTS er on er.R
     end if;
 end loop;
 */
-for rec in (select * from ABSENCE_GROUP_EMPLOYEE eal join EMPLOYEE_REQUESTS er on er.EMPLOYEE_CODE_REQ =  eal.EMPLOYEE_CODE where er.id = v_request_id and EXPIRATION_DATE >= to_char(sysdate,'MM/DD/YYYY'))loop
-    if (rec.CARRY_FORWARD + rec.PLAN_YEAR_ACCRUED) > 0 and rec.total_days <= (rec.CARRY_FORWARD + rec.PLAN_YEAR_ACCRUED) then
-    update EMPLOYEE_REQUESTS set emp_req_status = 3,man_res_status = 3 where id = v_request_id;
+
+/*for rec in (select * from ABSENCE_GROUP_EMPLOYEE eal join EMPLOYEE_REQUESTS er on er.EMPLOYEE_CODE_REQ =  eal.EMPLOYEE_CODE 
+where er.id = v_request_id and EXPIRATION_DATE >= to_char(sysdate,'MM/DD/YYYY'))loop
+    if rec.AVAILABLE > 0 and rec.total_days <= rec.AVAILABLE then*/
+    
+    /*
         if rec.total_days <= rec.CARRY_FORWARD then
-            update ABSENCE_GROUP_EMPLOYEE 
-            set CARRY_FORWARD_USED = rec.CARRY_FORWARD_USED + rec.total_days,
-            DAY_APPROVE = sysdate,
-            CARRY_FORWARD = rec.CARRY_FORWARD - rec.total_days
-            -- Create leave request API
-
-            for rec in (select * FROM EMPLOYEE_REQUESTS where id = v_request_id) loop
-                r_name_type := rec.NAME;
-                r_AdjustedHours := rec.TOTAL_DAYS;
-                r_AdjustmentType := rec.TYPE;
-                r_TransactionDate date := rec.FROM_DATE;
-                r_Description := rec.NOTE;
-                r_EmployeeCode := EMPLOYEE_CODE_REQ;
-                r_HRMAbsenceCodeId := rec.LEAVE_TYPE;
-                r_IDPortal := rec.ID;
-            end loop;
-            
-            SP_CREATE_LEAVER_REQUEST(
-                r_AdjustedHours, r_AdjustmentType, r_TransactionDate, r_Description, r_EmployeeCode, r_HRMAbsenceCodeId, r_IDPortal
-            );
-
-        where EMPLOYEE_CODE = rec.EMPLOYEE_CODE and EXPIRATION_DATE >= to_char(sysdate,'MM/DD/YYYY');
+            begin
+                update ABSENCE_GROUP_EMPLOYEE 
+                set CARRY_FORWARD_USED = rec.CARRY_FORWARD_USED + rec.total_days,
+                DAY_APPROVE = sysdate,
+                CARRY_FORWARD = rec.CARRY_FORWARD - rec.total_days
+                where EMPLOYEE_CODE = rec.EMPLOYEE_CODE and EXPIRATION_DATE >= to_char(sysdate,'MM/DD/YYYY');
+*/
+                for rec in (select * FROM EMPLOYEE_REQUESTS where id = v_request_id) loop
+                    r_AdjustedHours := rec.TOTAL_DAYS;
+                    r_AdjustmentType := 10;
+                    r_TransactionDate := rec.FROM_DATE;
+                    r_Description := rec.NOTE;
+                    r_EmployeeCode := rec.EMPLOYEE_CODE_REQ;
+                    r_HRMAbsenceCodeId := rec.LEAVE_TYPE;
+                    r_IDPortal := rec.ID;
+                end loop;
+                update EMPLOYEE_REQUESTS set emp_req_status = 3,man_res_status = 3 where id = v_request_id;
+                SP_CREATE_LEAVER_REQUEST(
+                    TO_CHAR(r_AdjustedHours), TO_CHAR(r_AdjustmentType), TO_CHAR(r_TransactionDate, 'MM-DD-YYYY'), r_Description, r_EmployeeCode, r_HRMAbsenceCodeId, TO_CHAR(r_IDPortal)
+                );
+                /*
+            end;
         else
             v_day_temp := rec.total_days - rec.CARRY_FORWARD;
             update ABSENCE_GROUP_EMPLOYEE 
@@ -74,7 +80,7 @@ for rec in (select * from ABSENCE_GROUP_EMPLOYEE eal join EMPLOYEE_REQUESTS er o
             CARRY_FORWARD = 0,
             CARRY_FORWARD_USED = rec.CARRY_FORWARD_USED + rec.CARRY_FORWARD
         where EMPLOYEE_CODE = rec.EMPLOYEE_CODE and EXPIRATION_DATE >= to_char(sysdate,'MM/DD/YYYY');
-        end if;
-    end if;
-end loop;
+        end if;*/
+    --end if;
+--end loop;
 end;
