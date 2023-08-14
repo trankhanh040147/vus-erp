c7843c0d96ffab09063efde019982c976bb8200c
trankhanh040147 <trankhanh040147@gmail.com>
2023-08-14 08:23:48 +0700

    Update Approve_Leave.sql

--- SQL QUERY/search_filter/Approve_Leave.sql
+++ SQL QUERY/search_filter/Approve_Leave.sql
@@ -10,18 +10,23 @@ select er.ID,
        rt.NAME as TYPE,
        er.TOTAL_DAYS,
        s.status as STATUS,
-        case er.emp_req_status
-        when 2 then
-        'Delete' 
-        else
-        null
-        end as Action
+       er.man_res_status as RESPONSE,
+       age.HRM_ABSENCE_CODE_ID,
+       age.HRM_ABSENCE_CODE_GROUP_ID,
+       case er.emp_req_status
+       when 2 then
+       'Aprrove' 
+       when 3 then
+       'Cancel'
+       else
+       null
+       end as Action
   from EMPLOYEE_REQUESTS er
-  join REQUEST_TYPE rt on er.LEAVE_TYPE = rt.REQUEST_TYPE_CODE
+  left join REQUEST_TYPE rt on er.LEAVE_TYPE = rt.REQUEST_TYPE_CODE
+  left join ABSENCE_GROUP_EMPLOYEE age on er.EMPLOYEE_CODE_REQ = age.EMPLOYEE_CODE
   join STATUS s on er.emp_req_status = s.id
-  where er.IS_DEL = 0 and er.REQUESTOR_ID = :APP_USER_ID
-  and to_date(SUBMIT_DATE,'MM/DD/YYYY') between to_date(:P7_FROM_DATE,'MM/DD/YYYY') and to_date(:P7_END_DATE,'MM/DD/YYYY')
-  and :P7_STATUS = 99
+  where er.IS_DEL = 0 and er.REQUESTOR_ID <> :APP_USER_ID --and er.man_res_status is null 
+  /*
   union
   select er.ID,
        er.REQUESTOR_ID,
@@ -35,19 +40,13 @@ select er.ID,
        rt.NAME as TYPE,
        er.TOTAL_DAYS,
        s.status as STATUS,
-        case er.emp_req_status
-        when 2 then
-        'Delete' 
-        else
-        null
-        end as Action
+       er.man_res_status as RESPONSE,
+       age.HRM_ABSENCE_CODE_ID,
+       age.HRM_ABSENCE_CODE_GROUP_ID,
+       null as Action
   from EMPLOYEE_REQUESTS er
-  join REQUEST_TYPE rt on er.LEAVE_TYPE = rt.REQUEST_TYPE_CODE
+  left join REQUEST_TYPE rt on er.emp_req_status = rt.id
+  left join ABSENCE_GROUP_EMPLOYEE age on er.EMPLOYEE_CODE_REQ = age.EMPLOYEE_CODE
   join STATUS s on er.emp_req_status = s.id
-  where er.IS_DEL = 0 and er.REQUESTOR_ID = :APP_USER_ID
-  and to_date(SUBMIT_DATE,'MM/DD/YYYY') between to_date(:P7_FROM_DATE,'MM/DD/YYYY') and to_date(:P7_END_DATE,'MM/DD/YYYY')
-  and :P7_STATUS = er.emp_req_status
-  and :P7_STATUS <> 99
-
-  
-
+  where er.IS_DEL = 0 and er.man_res_status is not null and er.REQUESTOR_ID <> :APP_USER_ID
+  */
