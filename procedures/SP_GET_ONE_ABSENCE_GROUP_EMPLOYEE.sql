49913317baf811c51f08791c94e36928437cb6b2
trankhanh040147 <trankhanh040147@gmail.com>
2023-08-21 08:00:09 +0700

    Update SP_GET_ONE_ABSENCE_GROUP_EMPLOYEE.sql

--- procedures/SP_GET_ONE_ABSENCE_GROUP_EMPLOYEE.sql
+++ procedures/SP_GET_ONE_ABSENCE_GROUP_EMPLOYEE.sql
@@ -15,6 +15,7 @@ create or replace PROCEDURE "SP_GET_ONE_ABSENCE_GROUP_EMPLOYEE" (
     l_body_json CLOB;
     n_id NUMBER;
     n_carryforward NUMBER;
+    n_can_cf NUMBER;
     n_planyearaccrued NUMBER;
     n_planyearused NUMBER;
     n_employeeCode NVARCHAR2(200);
@@ -43,6 +44,10 @@ BEGIN
     apex_web_service.g_request_headers(2).name := 'Content-Type';
     apex_web_service.g_request_headers(2).value := 'application/json; charset=utf-8';
 
+    -- Delete current Absence_groups of this employee OR update it
+    DELETE FROM ABSENCE_GROUP_EMPLOYEE WHERE EMPLOYEE_CODE = p_employee_code;
+
+
     FOR r IN (SELECT EMPLOYEE_CODE, DATAAREA FROM EMPLOYEES WHERE EMPLOYEE_CODE = p_employee_code) LOOP
         l_body := '{"_jsonRequest":{"EmployeeCode":"' || r.EMPLOYEE_CODE || '","legal_entity":"' || r.DATAAREA || '"} }';
 
@@ -70,7 +75,12 @@ BEGIN
             n_hrm_absence_code_id := apex_json.get_varchar2('Benefit_accrual[%d].HRMAbsenceCodeId', i);
             n_hrm_absence_code_group_id := apex_json.get_varchar2('Benefit_accrual[%d].HRMAbsenceCodeGroupId', i);
             n_expiration_date := TO_DATE(apex_json.get_varchar2('Benefit_accrual[%d].ExpirationDate', i), 'YYYY-MM-DD"T"HH24:MI:SS');
+            n_can_cf := apex_json.get_number('Benefit_accrual[%d].CanCarryForward', i);
 
+            -- Convert Group 'Leave' to 'APL'
+            if n_hrm_absence_code_group_id = 'Leave' then
+                n_hrm_absence_code_group_id := 'APL';
+            end if;
             DBMS_OUTPUT.put_line('Values:' || TO_CHAR(l_numrow) || ' n_benefit_accrual_plan:' || n_benefit_accrual_plan);
 
             -- Insert data into the table ABSENCE_GROUP_EMPLOYEE
@@ -89,7 +99,8 @@ BEGIN
                 AVAILABLE,
                 HRM_ABSENCE_CODE_ID,
                 HRM_ABSENCE_CODE_GROUP_ID,
-                EXPIRATION_DATE
+                EXPIRATION_DATE,
+                CAN_CARRY_FORWARD
             )
             VALUES (
                 l_max,
@@ -105,10 +116,49 @@ BEGIN
                 n_available,
                 n_hrm_absence_code_id,
                 n_hrm_absence_code_group_id,
-                n_expiration_date
+                n_expiration_date,
+                n_can_cf
             );
 
         END LOOP;
+
     END LOOP;
+
+
+    -- Merge AL & CF 
+    FOR r1 IN (SELECT * FROM ABSENCE_GROUP_EMPLOYEE WHERE EMPLOYEE_CODE = p_employee_code) LOOP 
+        IF r1.HRM_ABSENCE_CODE_GROUP_ID = 'APL' AND r1.CAN_CARRY_FORWARD = 0 THEN
+            FOR r2 IN (SELECT * FROM ABSENCE_GROUP_EMPLOYEE WHERE EMPLOYEE_CODE = p_employee_code) LOOP
+                IF r2.HRM_ABSENCE_CODE_GROUP_ID = 'APL' AND r2.CAN_CARRY_FORWARD = 1 THEN
+                    -- Perform the updates
+                    r1.CARRY_FORWARD := r1.CARRY_FORWARD + r2.CARRY_FORWARD;
+                    r1.CARRY_FORWARD_AVALABLE := r2.AVAILABLE;
+                    r1.CARRY_FORWARD_CODE := r2.HRM_ABSENCE_CODE_ID;
+                    r1.CARRY_FORWORD_EXP_DATE := r2.EXPIRATION_DATE;
+                    r1.CF_BENEFIT_ACCRUAL_PLAN := r2.BENEFIT_ACCRUAL_PLAN;
+
+                    -- Update r1 in database
+                    UPDATE ABSENCE_GROUP_EMPLOYEE
+                    SET CARRY_FORWARD = r1.CARRY_FORWARD,
+                        CARRY_FORWARD_AVALABLE = r1.CARRY_FORWARD_AVALABLE,
+                        CARRY_FORWARD_CODE = r1.CARRY_FORWARD_CODE,
+                        CARRY_FORWORD_EXP_DATE = r1.CARRY_FORWORD_EXP_DATE,
+                        CF_BENEFIT_ACCRUAL_PLAN = r1.CF_BENEFIT_ACCRUAL_PLAN
+                    WHERE EMPLOYEE_CODE = p_employee_code
+                    AND HRM_ABSENCE_CODE_GROUP_ID = 'APL'
+                    AND CAN_CARRY_FORWARD = 0;
+
+                    -- Delete r2 from database
+                    DELETE FROM ABSENCE_GROUP_EMPLOYEE WHERE EMPLOYEE_CODE = p_employee_code AND HRM_ABSENCE_CODE_GROUP_ID = 'APL' AND CAN_CARRY_FORWARD = 1;
+
+                    EXIT; -- Exit the inner loop after updating and deleting
+                END IF;
+            END LOOP;
+            
+            EXIT; -- Exit the outer loop after finding the first record
+        END IF;
+    END LOOP;
+
+
 END;
 /
