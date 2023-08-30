17194e207d15ca361ab2dbc2ace3a9d2c3efd358
trankhanh040147 <trankhanh040147@gmail.com>
2023-08-30 15:28:20 +0700

    Update submit request

--- SQL QUERY/leave_request/submit_employee.sql
+++ SQL QUERY/leave_request/submit_employee.sql
@@ -1,5 +1,6 @@
 declare
 v_request_id_temp number:=1;
+v_id NUMBER:=1;
 v_name_type nvarchar2(100);
 v_benefit_code nvarchar2(100):='';
 v_annual_temp float:=0;
@@ -32,21 +33,35 @@ for rec in ( select * from ABSENCE_GROUP_EMPLOYEE where employee_code = :APP_EMP
 end loop;
 
 select "REQUEST_ID".nextval into v_request_id_temp from sys.dual; 
-insert into EMPLOYEE_REQUESTS (
-    REQUESTOR_ID,REQUEST_ID,EMPLOYEE_CODE_REQ,EMPLOYEE_NAME,FROM_DATE,
-    END_DATE,ALL_DAY,NOTE,TOTAL_DAYS,EMP_REQ_STATUS,LEAVE_TYPE,RESPONSER_ID,TARGET_CODE,
-    START_TIME,END_TIME,SUBMIT_DATE,BENEFIT_CODE,CRF_DAY_TEMP,ANNUAL_DAY_TEMP,ATTACH_NAME,ATTATCH_FILE
+
+-- Insert leave request
+-- insert into EMPLOYEE_REQUESTS (
+--     REQUESTOR_ID,REQUEST_ID,EMPLOYEE_CODE_REQ,EMPLOYEE_NAME,FROM_DATE,
+--     END_DATE,ALL_DAY,NOTE,TOTAL_DAYS,EMP_REQ_STATUS,LEAVE_TYPE,RESPONSER_ID,TARGET_CODE,
+--     START_TIME,END_TIME,SUBMIT_DATE,BENEFIT_CODE,CRF_DAY_TEMP,ANNUAL_DAY_TEMP,ATTACH_NAME,ATTATCH_FILE
+--     ) values (
+--         :APP_USER_ID,v_request_id_temp,:P3_EMPLOYEE_CODE,:P3_EMPLOYEE,:P3_FROM_DATE,
+--         :P3_END_DATE,to_char(:P3_ALL_DAY),:P3_NOTE,:P3_TOTAL_DAYS,2,:P3_ANNUAL_LEAVE,:P3_APPROVED_MANAGER,:P3_MANAGER_CODE,
+--         :P3_START_TIME,:P3_END_TIME,sysdate,v_benefit_code,v_crf_temp,v_annual_temp,nvl(:P3_NAME_FILES,''),nvl(:P3_URL_FILES,'#')
+--         ); 
+--     returning ID into v_id;
+
+    -- Insert a leave request and retrieve the ID into v_id
+    insert into EMPLOYEE_REQUESTS (
+        REQUESTOR_ID,REQUEST_ID,EMPLOYEE_CODE_REQ,EMPLOYEE_NAME,FROM_DATE,
+        END_DATE,ALL_DAY,NOTE,TOTAL_DAYS,EMP_REQ_STATUS,LEAVE_TYPE,RESPONSER_ID,TARGET_CODE,
+        START_TIME,END_TIME,SUBMIT_DATE,BENEFIT_CODE,CRF_DAY_TEMP,ANNUAL_DAY_TEMP,ATTACH_NAME,ATTATCH_FILE
     ) values (
         :APP_USER_ID,v_request_id_temp,:P3_EMPLOYEE_CODE,:P3_EMPLOYEE,:P3_FROM_DATE,
         :P3_END_DATE,to_char(:P3_ALL_DAY),:P3_NOTE,:P3_TOTAL_DAYS,2,:P3_ANNUAL_LEAVE,:P3_APPROVED_MANAGER,:P3_MANAGER_CODE,
         :P3_START_TIME,:P3_END_TIME,sysdate,v_benefit_code,v_crf_temp,v_annual_temp,nvl(:P3_NAME_FILES,''),nvl(:P3_URL_FILES,'#')
-        );
+    ) returning ID into v_id;
 
 -- Send mail
 
 SELECT USER_NAME, FULL_NAME INTO manager_email, manager_name FROM EMPLOYEES WHERE (SELECT MANAGER_ID FROM EMPLOYEES WHERE EMPLOYEE_CODE = :P3_EMPLOYEE_CODE) = EMPLOYEE_CODE;
     
-    -- For employee
+    --For employee
 SP_SENDGRID_EMAIL('VUSERP-PORTAL@vus-etsc.edu.vn', :APP_USER_NAME, 'Leave Request Submitted Successfully', '<p> Dear '|| :P3_EMPLOYEE ||', </p>' ||
 '<p> Your leave request has been submitted successfully. Here are the details: </p>' ||
 '<p> Employee Code: '|| :P3_EMPLOYEE_CODE ||' </p>' ||
@@ -60,7 +75,7 @@ SP_SENDGRID_EMAIL('VUSERP-PORTAL@vus-etsc.edu.vn', :APP_USER_NAME, 'Leave Reques
 '<p> VUS </p>');
 
 
-    -- For manager
+    --For manager
 SP_SENDGRID_EMAIL('VUSERP-PORTAL@vus-etsc.edu.vn', manager_email, 'Leave Request Approval Required', '<p>Dear '|| manager_name ||',</p>' ||
     '<p>A leave request from '|| :P3_EMPLOYEE ||' requires your approval. Here are the details:</p>' ||
     '<p>Employee Code: '|| :P3_EMPLOYEE_CODE ||'</p>' ||
@@ -68,7 +83,7 @@ SP_SENDGRID_EMAIL('VUSERP-PORTAL@vus-etsc.edu.vn', manager_email, 'Leave Request
     '<p>From Date: '|| :P3_FROM_DATE ||'</p>' ||
     '<p>To Date: '|| :P3_END_DATE ||'</p>' ||
     '<br>' || 
-    '<p>Please log in to the leave management system to review and approve the request.</p>' ||
+    '<p>Please log in to the leave management system to review and approve the request. You can approve the leave by clicking on the following link: <a href=\"https://erp-uat.vus.edu.vn/ords/r/erp/erp/approve-leave?request_id=' || to_char(v_id) || '\"> link to approve the leave ↗.</a></p>' ||
     '<br>' || 
     '<p>If you have any questions or need further information, please contact the HR department.</p>' ||
     '<br>' || 
