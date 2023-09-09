96fb123780a1cf6847739164ee251b3091a566ff
trankhanh040147 <trankhanh040147@gmail.com>
2023-09-09 10:43:25 +0700

    Update submit_manager.sql

--- SQL QUERY/leave_request/submit_manager.sql
+++ SQL QUERY/leave_request/submit_manager.sql
@@ -34,13 +34,16 @@ if :P10_ANNUAL_LEAVE = 'APL' then
         end if;
     end loop;
 else 
-    for rec_2  in ( select * from ABSENCE_GROUP_EMPLOYEE 
-    where employee_code = :APP_EMP_CODE and HRM_ABSENCE_CODE_GROUP_ID = :P10_ANNUAL_LEAVE and HRM_ABSENCE_CODE_ID = :P10_LEAVE_DETAIL) 
-    loop
-            v_benefit_code := rec_2.BENEFIT_ACCRUAL_PLAN;
-            v_annual_temp := :P10_TOTAL_DAYS;
-    end loop;
+    -- for rec_2  in ( select * from ABSENCE_GROUP_EMPLOYEE 
+    -- where employee_code = :APP_EMP_CODE and HRM_ABSENCE_CODE_GROUP_ID = :P10_ANNUAL_LEAVE and HRM_ABSENCE_CODE_ID = :P10_LEAVE_DETAIL) 
+    -- loop
+    --         v_benefit_code := rec_2.BENEFIT_ACCRUAL_PLAN;
+    --         v_annual_temp := :P10_TOTAL_DAYS;
+    -- end loop;
+    v_annual_temp := :P10_TOTAL_DAYS;
+    v_benefit_code := :P10_LEAVE_DETAIL;
 end if;
+
 
 select "REQUEST_ID".nextval into v_request_id_temp from sys.dual; 
 insert into EMPLOYEE_REQUESTS (
@@ -49,7 +52,7 @@ insert into EMPLOYEE_REQUESTS (
     START_TIME,END_TIME,SUBMIT_DATE,BENEFIT_CODE,CRF_DAY_TEMP,ANNUAL_DAY_TEMP,ATTACH_NAME,ATTATCH_FILE
     ) values (
         :APP_USER_ID,v_request_id_temp,:P10_EMPLOYEE_CODE,:P10_EMPLOYEE,:P10_FROM_DATE,
-        :P10_END_DATE,to_char(:P10_ALL_DAY),:P10_NOTE,:P10_TOTAL_DAYS,2,:P10_ANNUAL_LEAVE,:P10_APPROVED_MANAGER,:P10_MANAGER_CODE,
+        CASE WHEN :P10_END_DATE IS NULL OR :P10_END_DATE = '' THEN :P10_FROM_DATE ELSE :P10_END_DATE END ,to_char(:P10_ALL_DAY),:P10_NOTE,:P10_TOTAL_DAYS,2,:P10_ANNUAL_LEAVE,:P10_APPROVED_MANAGER,:P10_MANAGER_CODE,
         :P10_START_TIME,:P10_END_TIME,sysdate,v_benefit_code,v_crf_temp,v_annual_temp,nvl(:P10_NAME_FILES,''),nvl(:P10_URL_FILES,'#')
         )  returning ID into v_id;
 
