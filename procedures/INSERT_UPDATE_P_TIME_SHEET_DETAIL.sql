024b00c9609f0bc10828e0ed25cd2acedfd0c8cd
trankhanh040147 <trankhanh040147@gmail.com>
2023-11-06 20:15:05 +0700

    up INSERT_UPDATE_P_TIME_SHEET_DETAIL, SP_GET_TIME_SHEET

--- procedures/INSERT_UPDATE_P_TIME_SHEET_DETAIL.sql
+++ procedures/INSERT_UPDATE_P_TIME_SHEET_DETAIL.sql
@@ -1,23 +1,40 @@
 create or replace PROCEDURE "INSERT_UPDATE_P_TIME_SHEET_DETAIL"
-
+(
+    date_from DATE,
+    date_to DATE
+)
 IS
 
 n_count_1 NUMBER;
 n_TS_ID NUMBER;
-
-wt_in1 nvarchar2;
-wt_out1 nvarchar2;
-wt_in2 nvarchar2;
-wt_out2 nvarchar2;
+n_current_in NUMBER;
+n_current_out NUMBER;
+n_wt_in1 varchar2(10);
+n_wt_out1 varchar2(10);
+n_wt_in2 varchar2(10);
+n_wt_out2 varchar2(10);
+n_wt_in1_line_id varchar2(50);
+n_wt_out1_line_id varchar2(50);
+n_wt_in2_line_id varchar2(50);
+n_wt_out2_line_id varchar2(50);
 
 BEGIN
 
     -- FOR rec_ets IN (SELECT EMPLOYEE_CODE, WORKER, PROFILE_DATE, TS_HEADER_REC_ID, TRANSFERRED, PROFILE_ID  FROM EMP_TIME_SHEET)
-    FOR rec_ets IN (SELECT *  FROM EMP_TIME_SHEET)
+    -- Loop DATE_TS in time (date_from, date_to)
+    FOR rec_ets IN (SELECT *  FROM EMP_TIME_SHEET WHERE PROFILE_DATE >= date_from AND PROFILE_DATE <= date_to)
     
     LOOP
+        -- DBMS_OUTPUT.put_line('');
         SELECT ID into n_TS_ID FROM P_TIME_SHEET pts where pts.TS_HEADER_ID = rec_ets.TS_HEADER_REC_ID;
         SELECT COUNT(*) into n_count_1 FROM P_TIME_SHEET_DETAIL ptsd WHERE ptsd.TS_HEADER_ID = rec_ets.TS_HEADER_REC_ID;
+
+        -- reset time variables
+        n_wt_in1 := NULL;
+        n_wt_out1 := NULL;
+        n_wt_in2 := NULL;
+        n_wt_out2 := NULL;
+        
 
         -- Insert/Update P_TIME_SHEET_DETAIL(WT_STANDARD_HOURS,WT_FLEX,WT_RUSH,WT_OT)
         if n_count_1 > 0 then
@@ -26,9 +43,10 @@ BEGIN
                 P_TIME_SHEET_DETAIL ptsd
             SET
                 WT_STANDARD_HOURS = rec_ets.PAY_TIME,
-                WT_FLEX = rec_ets.FLEX_MORE + rec_ets.FLEX_LESS,
+                WT_FLEX = rec_ets.FLEX_MORE - rec_ets.FLEX_LESS,
                 -- WT_RUSH = rec_ets.PAY_TIME,
-                WT_OT = rec_ets.PAY_OVERTIME
+                WT_OT = rec_ets.PAY_OVERTIME,
+                APPROVED_LEAVES = rec_ets.ABSENCE_TIME
             WHERE
                 ptsd.TS_HEADER_ID = rec_ets.TS_HEADER_REC_ID;
         else
@@ -38,13 +56,15 @@ BEGIN
                 WT_FLEX,
                 -- WT_RUSH,
                 WT_OT,
+                APPROVED_LEAVES,
                 TS_HEADER_ID
             )
             VALUES(
                 n_TS_ID,
                 rec_ets.PAY_TIME,
-                rec_ets.FLEX_MORE + rec_ets.FLEX_LESS,
+                rec_ets.FLEX_MORE - rec_ets.FLEX_LESS,
                 rec_ets.PAY_OVERTIME,
+                rec_ets.ABSENCE_TIME,
                 rec_ets.TS_HEADER_REC_ID
             );
         END IF;
@@ -56,18 +76,66 @@ BEGIN
         for rec_etsl in (SELECT * FROM EMP_TIME_SHEET_LINE etsl where etsl.TS_HEADER_REC_ID = rec_ets.TS_HEADER_REC_ID order by etsl.TS_LINE_REC_ID)
         LOOP
             if rec_etsl.reg_type like '%clock in%' then
-                if n_current_in = 1 then wt_in1 := rec_etsl.start_time end if
-                if n_current_in = 2 then wt_in2 := rec_etsl.start_time end if
+                if n_current_in = 1 then n_wt_in1 := rec_etsl.start_time; n_wt_in1_line_id := rec_etsl.TS_LINE_REC_ID; end if;
+                if n_current_in = 2 then n_wt_in2 := rec_etsl.start_time; n_wt_in2_line_id := rec_etsl.TS_LINE_REC_ID; end if;
                 n_current_in := n_current_in + 1;
             end if;
             if rec_etsl.reg_type like '%clock out%' then
-                if n_current_out = 1 then wt_out1 := rec_etsl.start_time end if;
-                if n_current_out = 2 then wt_out2 := rec_etsl.start_time end if;
+                if n_current_out = 1 then n_wt_out1 := rec_etsl.start_time; n_wt_out1_line_id:= rec_etsl.TS_LINE_REC_ID; end if;
+                if n_current_out = 2 then n_wt_out2 := rec_etsl.start_time; n_wt_out2_line_id:= rec_etsl.TS_LINE_REC_ID; end if;
                 n_current_out := n_current_out + 1;
             end if;
         END LOOP;
 
-        COMMIT;
+        -- DBMS_OUTPUT.put_line('');
+        -- DBMS_OUTPUT.put_line(rec_ets.TS_HEADER_REC_ID);
+
+    -- P_TIME_SHEET_DETAIL
+    -- ID	NUMBER(6,0)
+    -- TS_ID	NUMBER(6,0)
+    -- WT_IN1	VARCHAR2(10 CHAR)
+    -- WT_OUT1	VARCHAR2(10 CHAR)
+    -- WT_IN2	VARCHAR2(10 CHAR)
+    -- WT_OUT2	VARCHAR2(10 CHAR)
+    -- WT_STANDARD_HOURS	NUMBER(5,2)
+    -- WT_FLEX	NUMBER(5,2)
+    -- WT_RUSH	NUMBER(5,2)
+    -- WT_OT	NUMBER(5,2)
+    -- AT_IN1	VARCHAR2(10 CHAR)
+    -- AT_OUT1	VARCHAR2(10 CHAR)
+    -- AT_IN2	VARCHAR2(10 CHAR)
+    -- AT_OUT2	VARCHAR2(10 CHAR)
+    -- AT_STANDARD_HOURS	NUMBER(5,2)
+    -- AT_FLEX	NUMBER(5,2)
+    -- AT_RUSH	NUMBER(5,2)
+    -- AT_OT	NUMBER(5,2)
+    -- IN1_TS_LINE_ID	VARCHAR2(30 CHAR)
+    -- OUT1_TS_LINE_ID	VARCHAR2(30 CHAR)
+    -- IN2_TS_LINE_ID	VARCHAR2(30 CHAR)
+    -- OUT2_TS_LINE_ID	VARCHAR2(30 CHAR)
+    -- TS_HEADER_ID	NVARCHAR2(30 CHAR)
+    -- APPROVED_LEAVES	NUMBER(5,2)
+    -- APPROVED_OT	NUMBER(5,2)
+
+        -- Update timesheet details with in and out times
+        UPDATE P_TIME_SHEET_DETAIL ptsd
+        SET
+            ptsd.WT_IN1 = n_wt_in1,
+            ptsd.WT_OUT1 = n_wt_out1,
+            ptsd.WT_IN2 = n_wt_in2,
+            ptsd.WT_OUT2 = n_wt_out2,
+            ptsd.IN1_TS_LINE_ID = n_wt_in1_line_id,
+            ptsd.OUT1_TS_LINE_ID = n_wt_out1_line_id,
+            ptsd.IN2_TS_LINE_ID = n_wt_in2_line_id,
+            ptsd.OUT2_TS_LINE_ID = n_wt_out2_line_id
+            -- ptsd.AT_IN1 = CASE WHEN ptsd.AT_IN1 IS NOT NULL THEN n_wt_in1 ELSE ptsd.AT_IN1 END,
+            -- ptsd.AT_OUT1 = CASE WHEN ptsd.AT_OUT1 IS NOT NULL THEN n_wt_out1 ELSE ptsd.AT_OUT1 END,
+            -- ptsd.AT_IN2 = CASE WHEN ptsd.AT_IN2 IS NOT NULL THEN n_wt_in2 ELSE ptsd.AT_IN2 END,
+            -- ptsd.AT_OUT2 = CASE WHEN ptsd.AT_OUT2 IS NOT NULL THEN n_wt_out2 ELSE ptsd.AT_OUT2 END
+        WHERE ptsd.TS_HEADER_ID = rec_ets.TS_HEADER_REC_ID;
+
+
+        -- COMMIT;
     END LOOP;
 END "INSERT_UPDATE_P_TIME_SHEET_DETAIL";
 /
